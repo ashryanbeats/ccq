890add1a5685746af807448cf96e7ef7c15fe1c4
Ash <ash@ashryanbeats.com>
2017-06-06 14:52:04 -0400

    Checking to see if app is running before attempting to quit.

--- ccq.sh
+++ ccq.sh
@@ -2,41 +2,47 @@
 script=$BASH_SOURCE
 path=${script%/*}
 filename="/mikael-cho-214358.jpg"
+appnamefull="Adobe Photoshop CC 2017"
+appnameshort="Photoshop CC"
 
-if [ -z "$1" ]
-	then
-    osascript -e 'quit app "Photoshop CC"'
+if pgrep -xq -- "${appnamefull}"; then
+	if [ -z "$1" ]
+		then
+			cmd="quit app \"$appnameshort\""
+			echo "$cmd"
+	    osascript -e "$cmd"
 
-    # This while loop doesn't work because PS is purged from launchctl immediately
-    # while [ '$(launchctl list | grep "Photoshop")' != "" ]
-    # do
-    #   echo "Waiting..."
-    #   echo "'$(launchctl list | grep "Photoshop")'"
-    #   break
-    # done
+	    # This while loop doesn't work because PS is purged from launchctl immediately
+	    # while [ '$(launchctl list | grep "Photoshop")' != "" ]
+	    # do
+	    #   echo "Waiting..."
+	    #   echo "'$(launchctl list | grep "Photoshop")'"
+	    #   break
+	    # done
 
-    # This is an inelegant workaround for issue with the above while loop
-    sleep 8
+	    # This is an inelegant workaround for issue with the above while loop
+	    sleep 8
+	fi
+
+	while [ "$1" != "" ] # loop through args
+	do
+	  ARG="$1"
+
+	  case $ARG in
+	    -f) # force
+	      kill -9 $(pgrep "${appnameshort}")
+
+				sleep 2
+
+	      shift
+	      ;;
+
+	    *) # spin through any other args
+				shift
+				;;
+
+		esac
+	done
 fi
 
-while [ "$1" != "" ] # loop through args
-do
-  ARG="$1"
-
-  case $ARG in
-    -f) # force
-      kill -9 $(pgrep Photoshop)
-
-			sleep 2
-
-      shift
-      ;;
-
-    *) # spin through any other args
-			shift
-			;;
-
-	esac
-done
-
-open -a "Adobe Photoshop CC 2017"  $path$filename
+open -a "${appnamefull}" $path$filename
