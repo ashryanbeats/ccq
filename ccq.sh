344fdfefeb4dad3ab3f472426a1d8809da175052
Ash <ash@ashryanbeats.com>
2018-03-06 13:00:04 -0500

    Removed the devtools opener code.

--- ccq.sh
+++ ccq.sh
@@ -49,19 +49,20 @@ open -a "${appnamefull}" $path$filename
 
 sleep 6
 
-# The following opens a location in the default browser. Chrome is my default browser this works for me. 
+# The following opens a location in the default browser. Chrome is my default browser this works for me.
 # I ran into a very complicated error with Sierra being unable to find available browsers, so I don't invoke Chrome directly here.
 osascript -e 'open location "http://localhost:8088"
-			tell application "Google Chrome" to activate
-			delay 1
-			tell application "System Events" to keystroke "j" using {option down, command down}
-			tell application "Google Chrome" to activate
-			delay 1
-			tell application "System Events" to keystroke tab
-			delay 1
-			tell application "System Events" to keystroke return'
+			tell application "Google Chrome" to activate'
 echo "done!"
 
 # Optionally, activate Photoshop again to bring it to the foreground:
 #osascript -e 'activate application "${appnamefull}"'
 
+## The following was intended to open devtools, but it actually opens the debugger for devtools.
+## Removed the code, but fyi in case we ever try to chase this down.
+# tell application "System Events" to keystroke "j" using {option down, command down}
+# tell application "Google Chrome" to activate
+# delay 1
+# tell application "System Events" to keystroke tab
+# delay 1
+# tell application "System Events" to keystroke return'
